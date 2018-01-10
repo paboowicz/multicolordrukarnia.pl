301273af1e91d279d5f6cb04712d280898a21f78
Sebastian Lechowicz <paboowicz@gmail.com>
2018-01-10 21:06:51 +0100

    podmiana fotki z tablica, zmiana tresci w index i przetargi

--- przetargi.php
+++ przetargi.php
@@ -2,7 +2,18 @@
 
 <h1>Przetargi</h1>
 <hr>
-<p><b>„Budowa nowego zakładu do produkcji etykiet wielowarstwowych”, Projekt realizowany w ramach Regionalnego Programu Operacyjnego Województwa Podkarpackiego na lata 2014-2020, Oś priorytetowa I Konkurencyjna i innowacyjna gospodarka, Działanie: 1.4 Wsparcie MŚP, Poddziałanie: 1.4.1 Dotacje bezpośrednie, Wniosek o dofinansowanie Projektu nr: RPPK.01.04.01-18-0397/16 z dnia 29 kwietnia 2016 r.</b></p>
+<p>„Budowa nowego zakładu do produkcji etykiet wielowarstwowych”, Projekt realizowany w ramach
+Regionalnego Programu Operacyjnego Województwa Podkarpackiego na lata 2014-2020, Oś
+priorytetowa I Konkurencyjna i innowacyjna gospodarka, Działanie: 1.4 Wsparcie MŚP, Poddziałanie:
+1.4.1 Dotacje bezpośrednie, Umowa o dofinansowanie Projektu nr: RPPK.01.04.01-18-0397/16 z dnia
+29 kwietnia 2016 r.</p>
+<p>Źródło finansowania Europejski Fundusz Rozwoju Regionalnego w ramach Regionalnego Programu
+Operacyjnego Województwa Podkarpackiego na lata 2014-2020, Oś priorytetowa I Konkurencyjna i
+innowacyjna gospodarka, Działanie: 1.4 Wsparcie MŚP, Poddziałanie: 1.4.1 Dotacje bezpośrednie,
+Umowa o dofinansowanie Projektu nr: RPPK.01.04.01-18-0397/16 z dnia 29 kwietnia 2016 r.</p>
+<p>Planowany efekt:w wyniku realizacji projektu firma wdroży nowy produkt w postaci etykiet booklet.</p>
+<p>Wartość projektu:11.647.395,36 zł</p>
+<p>Dofinansowanie z UE:6.490.249,99zł</p>
 <hr>
 <p><b>Firma Zakład Wielobranżowy Drukarnia „Mulicolor” Bujak Krzysztof z siedzibą w Jarosławiu w związku z ubieganiem się o dofinansowanie projektu w Polskiej Agencji Rozwoju Przedsiębiorczości, zaprasza niniejszym do złożenia oferty dotyczącej realizacji zamówienia w postaci przeprowadzenia audytu wzorniczego dla firmy Zakład Wielobranżowy Drukarnia „Mulicolor” Bujak Krzysztof wraz z opracowaniem strategii wzorniczej, na potrzeby projektu pn. „Audyt wzorniczy i strategia wzornicza w firmie „Multicolor” Krzysztof Bujak czynnikiem rozwoju i wzrostu konkurencyjności.”, w ramach Programu Operacyjnego Polska Wschodnia 2014-2020, Oś Priorytetowa I: Przedsiębiorcza Polska Wschodnia, Działanie 1.4 Wzór na konkurencję I Etap, Numer konkurs: 1, Rok: 2017.
 </b></p>
